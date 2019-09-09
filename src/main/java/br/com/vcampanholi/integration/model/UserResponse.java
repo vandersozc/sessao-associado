719a25867cfc883822876a9aeed473459b5a654c
Vanderson Campanholi <vando.zc@gmail.com>
2019-09-08 22:13:17 -0300

    Tarefa bonus 01

---
+++ src/main/java/br/com/vcampanholi/integration/model/UserResponse.java
@@ -0,0 +1,1 @@
+package br.com.vcampanholi.integration.model;import lombok.AllArgsConstructor;import lombok.Builder;import lombok.Data;import lombok.NoArgsConstructor;@Data@Builder@AllArgsConstructor@NoArgsConstructorpublic class UserResponse {    private UserStatusEnum status;}
